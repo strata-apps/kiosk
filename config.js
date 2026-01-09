c29be19102798fb51a63b3fd8f3df67af526b037
darian-benitezsanchez <darian.benitez@campcatanese.org>
2026-01-09 16:14:54 -0700

    first commit

--- config.js
+++ config.js
@@ -6,7 +6,7 @@ export const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiO
 
 // Kiosk branding
 export const KIOSK_BRAND = {
-  name: "ReachPoint",
+  name: "ReachPoint Kiosk",
   // Use any image URL or local path in /assets
   logoUrl: "https://www.campcatanese.com/uploads/3/9/2/1/39218901/published/untitled-design.png?1700610909",
 };
